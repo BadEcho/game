cd752b957a89213352baee73dbfa7f33660c35e0
Matt Weber <matt@badecho.com>
2023-08-24 13:16:55 -0500

    Test file for MSDF assets

--- src/Game.Pipeline/DistanceFieldFonts/DistanceFieldFontAsset.cs
+++ src/Game.Pipeline/DistanceFieldFonts/DistanceFieldFontAsset.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2023 Bad Echo LLC. All rights reserved.
+//
+//      Bad Echo Technologies are licensed under the
+//      GNU Affero General Public License v3.0.
+//
+//      See accompanying file LICENSE.md or a copy at:
+//      https://www.gnu.org/licenses/agpl-3.0.html
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,4 +24,11 @@ namespace BadEcho.Game.Pipeline.DistanceFieldFonts;
 /// </summary>
 public sealed class DistanceFieldFontAsset
 {
+    /// <summary>
+	/// Initializes a new instance of the <see cref="DistanceFieldFontAsset"/> class.
+	/// </summary>
+    public DistanceFieldFontAsset()
+    {
+
+    }
 }
