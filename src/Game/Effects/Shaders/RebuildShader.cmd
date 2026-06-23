5450cb50f8bb127d34ebe8e8940921c64e4345c9
Matt Weber <matt@BadEcho.com>
2026-06-22 23:52:54 -0500

    Working on incremental shader rebuild
    Works, but will rebuild every time...will need to see how to fix

---
+++ src/Game/Effects/Shaders/RebuildShader.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+set name=%1
+set nameWithoutExt=%name%:~0,-3
+set skipVulcan=
+
+if "%2"==""  set skipVulcan=true
+if "%2"==true set skipVulcan=true
+
+cd /d %~dp0\..\..\
+dotnet tool run mgfxc %name% %nameWithoutExt%.ogl.mgfxo /profile:OpenGL /Debug
+dotnet tool run mgfxc %name% %nameWithoutExt%.dx11.mgfxo /profile:DirectX_11 /Debug
+
+if defined skipVulcan goto :create-resources
+
+dotnet tool run mgfxc %name% %nameWithoutExt%.vulkan.mgfxo /profile:Vulkan /Debug
+
+:create-resources
+dotnet tool run resource-creator . -f *.mgfxo -o ..\Shaders
