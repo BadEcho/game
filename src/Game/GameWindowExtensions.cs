31fc864833158201ba88a11e80bf8dea395406af
Matt Weber <matt@BadEcho.com>
2026-09-08 22:08:56 -0500

    Add handle retrieval for DX12

--- src/Game/GameWindowExtensions.cs
+++ src/Game/GameWindowExtensions.cs
@@ -15,6 +15,7 @@ using System.Runtime.InteropServices.Marshalling;
 using BadEcho.Extensions;
 using BadEcho.Game.Interop;
 using BadEcho.Game.Properties;
+using BadEcho.Interop;
 using Microsoft.Xna.Framework;
 using MonoGame.Framework.Utilities;
 
@@ -30,19 +31,34 @@ public static class GameWindowExtensions
     /// </summary>
     /// <param name="window">The game window to get the underlying window's handle for.</param>
     /// <returns>The handle to the underlying native window for <c>window</c>.</returns>
-    public static nint GetNativeWindowHandle(this GameWindow window)
+    public static WindowHandle GetNativeWindowHandle(this GameWindow window)
     {
         Require.NotNull(window, nameof(window));
 
         return PlatformInfo.MonoGamePlatform switch
         {
             MonoGamePlatform.DesktopGL => GetSdlWindowHandle(window),
-            MonoGamePlatform.Windows => window.Handle,
+            MonoGamePlatform.Windows => new WindowHandle(window.Handle, false),
+            MonoGamePlatform.WindowsDX12 => FindWindowHandle(),
             _ => throw new InvalidOperationException()
         };
     }
 
-    private static unsafe nint GetSdlWindowHandle(GameWindow window)
+    private static WindowHandle FindWindowHandle()
+    {   // The native MGRuntime.dll does not export the internal SDL window handle it creates. We have the handle to the MGP_WINDOW
+        // but that does us no good here. Without a clean way to extract the actual window handle, we're left with simply searching for it.
+        IEnumerable<NativeWindow> processWindows = NativeWindow.FromProcessId(Environment.ProcessId);
+
+        foreach (NativeWindow processWindow in processWindows)
+        {   // There tends to be more than one window associated with the process. Checking the class name will tell us for sure if it is the SDL window.
+            if (processWindow.ClassName == "SDL_app")
+                return processWindow.Handle;
+        }
+
+        return WindowHandle.InvalidHandle;
+    }
+
+    private static unsafe WindowHandle GetSdlWindowHandle(GameWindow window)
     {
         SdlWindowManagerInfo wmInfo = default;
         bool success = Sdl2.GetWindowManagerInfo(window.Handle, ref wmInfo);
@@ -57,6 +73,6 @@ public static class GameWindowExtensions
             throw new InvalidOperationException(Strings.SdlGetWindowManagerFailed.InvariantFormat(error));
         }
 
-        return wmInfo.window;
+        return new WindowHandle(wmInfo.window, false);
     }
 }
